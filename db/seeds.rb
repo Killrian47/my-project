76babc8d48ecfac551d9b4141d3295cec3c98bda
Killian Portier <killian.portier@yahoo.fr>
2024-05-24 21:49:33 +0200

    Feature: create seed for my database and add 30 elements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,33 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'open-uri'
+require 'json'
+
+# Seed for the database
+
+# Clean the database
+puts "Cleaning the database..."
+Cat.destroy_all if Rails.env.development?
+puts "Database cleaned!"
+
+# Create many cats with an API
+url = "https://api.thecatapi.com/v1/breeds"
+response = URI.open(url).read
+data = JSON.parse(response)
+
+puts "Creating cats..."
+index = 0
+data.each do |cat|
+  break if index == 30
+  img_url = "https://api.thecatapi.com/v1/images/#{cat["reference_image_id"]}"
+  img_rep = URI.open(img_url).read
+  img_data = JSON.parse(img_rep)
+
+  Cat.create!(
+    name: cat["name"],
+    picture: img_data["url"],
+    origin: cat["origin"],
+    description: cat["description"],
+    temperament: cat["temperament"]
+  )
+  index += 1
+end
+puts "Cats created!"
